b0975d76d3c009328a41c3cbc09f890fbe4b03cd
Prabhalika2000 <prabhalikapalli@incrivelsoft.com>
2025-03-11 14:34:18 +0530

    supporter page backend completed

--- user_management_service/model/momsurvey.js
+++ user_management_service/model/momsurvey.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const UserDetails= require('./User')
 const surveySchema = new mongoose.Schema(
   {
     generalDetails: {
@@ -38,5 +38,60 @@ const surveySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const supporterSchema =new mongoose.Schema(
+  {
+   user_name: { 
+    type: String,
+    ref: "UserDetails",
+    required: true }, 
+    name:{
+      type:String,
+      required:true,
+    },
+    relationship:{
+      type:String,
+      required:true,
+    },
+    hasAccess:{
+      type:Boolean,
+      default:false,
+    },
+    accessPermissions:{
+      pregnancyTracker:{
+        type:Boolean,
+        defalut:false,
+      },
+      dailyJournal:{
+        type:Boolean,
+        defalut:false,
+      },
+      appointment:{
+        type:Boolean,
+        default:false,
+      },
+      babyNames:{
+        type:Boolean,
+        default:false,
+      },
+    },
+    referralPin:{
+        type:String,
+        unique:true,
+      },
+},{timestamps:true}
+);
+
+
+// 🔹 Pre-save hook to generate a unique referralPin
+supporterSchema.pre("save", function (next) {
+  if (!this.referralPin) {
+    this.referralPin = `SUPP-${Math.random().toString(36).substr(2, 6).toUpperCase()}`;
+  }
+  next();
+});
+
+
 const Survey = mongoose.model("Survey", surveySchema);
-module.exports = Survey;
+
+const SupporterSurvey =mongoose.model("SupporterSurvey",supporterSchema)
+module.exports = {Survey,SupporterSurvey};
