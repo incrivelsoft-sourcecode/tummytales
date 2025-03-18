324d2dbcd3efb06860497caf7c19c284cc04b28d
unknown <Balasubrahmanyam@incrivelsoft.com>
2025-03-18 20:22:58 +0530

    redirect uri changed

--- user_management_service/utils/passport.js
+++ user_management_service/utils/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5001/users/google/callback",
+      callbackURL: "https://user-service-rt6l.onrender.com/users/google/callback",
       passReqToCallback: true, // ✅ Allows access to req in callback
     },
     async (req, accessToken, refreshToken, profile, done) => {
