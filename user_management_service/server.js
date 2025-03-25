73efe7bb46fa42712f6753781f90561d5f697172
unknown <Balasubrahmanyam@incrivelsoft.com>
2025-03-25 22:40:53 +0530

    cors

--- user_management_service/server.js
+++ user_management_service/server.js
@@ -15,12 +15,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
-
+app.use(cors({ origin: "https://tummytales-g87f.vercel.app/" }));
 
 
 app.use(express.json({ limit: '6mb' }));
