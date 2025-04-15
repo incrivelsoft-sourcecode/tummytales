6bc018fef9a8b83c1d03fa1137c8d1771e409fd4
incrivelsoft-sourcecode <pdulam@incrivelsoft.com>
2025-04-15 18:56:02 +0530

    Update server.js

--- mail_service/server.js
+++ mail_service/server.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://54.163.147.226:3000";
 
 // Middleware
-app.use(cors());
+app.use(cors("http://54.163.147.226:5001"));
 app.use(express.json());
 
 // Nodemailer transporter
