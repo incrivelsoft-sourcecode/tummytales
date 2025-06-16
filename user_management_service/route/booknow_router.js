3891269977fd27087ff258fd16e951d7b9b7c46e
incrivelsoft-sourcecode <pdulam@incrivelsoft.com>
2025-06-16 21:50:07 +0530

    Update booknow_router.js

--- user_management_service/route/booknow_router.js
+++ user_management_service/route/booknow_router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {momAndSupporterMiddleware,authorizeMommiddleware} =require('../middleware/authMiddleware')
 const booknow_router = express.Router();
+
 const {
   createBookNow,
   getBookNow,
