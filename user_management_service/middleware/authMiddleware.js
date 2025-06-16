45d28a3f3f9d20353783c55a8357f312931b4902
Prabhalika2000 <prabhalikapalli@incrivelsoft.com>
2025-06-16 21:54:29 +0530

    fb env

--- user_management_service/middleware/authMiddleware.js
+++ user_management_service/middleware/authMiddleware.js
@@ -39,7 +39,7 @@ const supporterMiddleware = (req, res, next) => {
 		res.status(401).json({ message: "Token is not valid" });
 	}
 };
-
+//main
 const momAndSupporterMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
   if (!token) {
