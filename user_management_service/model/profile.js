3ad33a8d876a63e76875aaaecfe0e7712e62f70c
Prabhalika2000 <prabhalikapalli@incrivelsoft.com>
2025-07-31 21:24:37 +0530

    profile

--- user_management_service/model/profile.js
+++ user_management_service/model/profile.js
@@ -106,9 +106,9 @@ const surveySchema = new mongoose.Schema(
       // physical activity fileds
       heightCm: Number, // Height (cm)
       weightKg: Number, // Weight (kg)
-      prePregnancyFitnessLevel: String, // Pre-pregnancy fitness level
-      preExistingConditions:String,
-      preferredExercises:String,
+      prePregnancyFitnessLevel: [String],
+      preExistingConditions: [String],
+      preferredExercises: [String],      
     },
     experienceAndExpectations: {
       expectations: String,
