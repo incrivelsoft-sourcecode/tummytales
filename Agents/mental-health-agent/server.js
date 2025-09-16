c0e633378d68782bc97788061e97b92328b629a4
incrivelsoft-sourcecode <pdulam@incrivelsoft.com>
2025-09-16 21:35:13 +0530

    Update server.js

--- Agents/mental-health-agent/server.js
+++ Agents/mental-health-agent/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+
 const quizRoutes = require('./routes/quizRoutes');
 const agentRoutes = require('./routes/agentRoutes');
 
